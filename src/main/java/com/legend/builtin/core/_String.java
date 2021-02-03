1955b30cdc07228166dfa9d5f3a3499f7ac0b183
Xchuanshuo <2414605975@qq.com>
2021-02-03 13:50:36 +0800

    update

--- src/main/java/com/legend/builtin/core/_String.java
+++ src/main/java/com/legend/builtin/core/_String.java
@@ -5,6 +5,7 @@ import com.legend.builtin.Registry;
 import com.legend.gen.operand.Register;
 import com.legend.semantic.Class;
 import com.legend.vm.Object;
+import com.legend.vm.Slot;
 import com.legend.vm.Slots;
 import com.legend.vm.StringPool;
 
@@ -18,12 +19,14 @@ public class _String extends Class {
     private static _String string = new _String();
 
     public static void bind() {
-        Registry.register(Registry.DEFAULT, "len(ST,)I",
-                new BuiltInFunction(string, "length"));
-        Registry.register(Registry.DEFAULT, "strAt(ST,I,)ST",
-                new BuiltInFunction(string, "strAt"));
-        Registry.register(Registry.DEFAULT, "equals(ST,ST,)Z",
-                new BuiltInFunction(string, "equals"));
+        Registry.register(Registry.DEFAULT, "len(ST,)I", new BuiltInFunction(string, "length"));
+        Registry.register(Registry.DEFAULT, "strAt(ST,I,)ST", new BuiltInFunction(string, "strAt"));
+        Registry.register(Registry.DEFAULT, "equals(ST,ST,)Z", new BuiltInFunction(string, "equals"));
+        Registry.register(Registry.DEFAULT, "equals(ST,ST,)Z", new BuiltInFunction(string, "equals"));
+        Registry.register(Registry.DEFAULT, "replace(ST,ST,ST,)ST", new BuiltInFunction(string, "replace"));
+        Registry.register(Registry.DEFAULT, "replaceAll(ST,ST,ST,)ST", new BuiltInFunction(string, "replaceAll"));
+        Registry.register(Registry.DEFAULT, "indexOf(ST,ST,)I", new BuiltInFunction(string, "indexOf"));
+        Registry.register(Registry.DEFAULT, "contains(ST,ST,)Z", new BuiltInFunction(string, "contains"));
     }
 
     public _String() {
@@ -50,5 +53,43 @@ public class _String extends Class {
         Object ref2 = args.getRef( 1);
         String str2 = StringPool.getString(ref2);
         regs.setBoolean(Register.RV, str1.equals(str2));
+    }
+
+    public void replace(Slots args, Slots regs) {
+        Object ref1 = args.getRef(0);
+        Object ref2 = args.getRef(1);
+        Object ref3 = args.getRef(2);
+        String str1 = StringPool.getString(ref1);
+        String str2 = StringPool.getString(ref2);
+        String str3 = StringPool.getString(ref3);
+        String res = str1.replace(str2, str3);
+        regs.setRef(Register.RV, StringPool.getStrObj(res));
+    }
+
+    public void replaceAll(Slots args, Slots regs) {
+        Object ref1 = args.getRef(0);
+        Object ref2 = args.getRef(1);
+        Object ref3 = args.getRef(2);
+        String str1 = StringPool.getString(ref1);
+        String str2 = StringPool.getString(ref2);
+        String str3 = StringPool.getString(ref3);
+        String res = str1.replaceAll(str2, str3);
+        regs.setRef(Register.RV, StringPool.getStrObj(res));
+    }
+
+    public void contains(Slots args, Slots regs) {
+        Object ref1 = args.getRef(0);
+        Object ref2 = args.getRef(1);
+        String str1 = StringPool.getString(ref1);
+        String str2 = StringPool.getString(ref2);
+        regs.setBoolean(Register.RV, str1.contains(str2));
+    }
+
+    public void indexOf(Slots args, Slots regs) {
+        Object ref1 = args.getRef(0);
+        Object ref2 = args.getRef(1);
+        String str1 = StringPool.getString(ref1);
+        String str2 = StringPool.getString(ref2);
+        regs.setInt(Register.RV, str1.indexOf(str2));
     }
 }
--- src/main/java/com/legend/vm/StringPool.java
+++ src/main/java/com/legend/vm/StringPool.java
@@ -4,6 +4,10 @@ import com.legend.common.MetadataArea;
 import com.legend.semantic.Class;
 import com.legend.semantic.PrimitiveType;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 /**
  * @author Legend
  * @data by on 21-1-24.
@@ -11,11 +15,17 @@ import com.legend.semantic.PrimitiveType;
  */
 public class StringPool {
 
+    private static Map<String,Object> map = new HashMap<>();
+
     public static Object getStrObj(String value) {
+        if (map.containsKey(value)) {
+            return map.get(value);
+        }
         Class clazz = (Class) MetadataArea.getInstance().
                 getTypeByName(PrimitiveType.String.name());
         Object strObj = clazz.newObj();
         strObj.setData(value);
+        map.put(value, strObj);
         return strObj;
     }
 
